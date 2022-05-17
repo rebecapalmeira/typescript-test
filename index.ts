95a7986568690605ea79275e27b92bceead50d0f
rebecapalmeira <rebeca.palmeira@gmail.com>
2022-05-17 20:00:28 -0300

    register practice with type system

--- index.ts
+++ index.ts
@@ -11,14 +11,15 @@ const url = 'https://jsonplaceholder.typicode.com/todos/1';
 axios.get(url).then(response => {
     const {id, title, completed} = response.data as ToDo;
 
-    logToDo(id, completed, title)
+    logToDo(id, title, completed)
 });
 
-const logToDo = (id: number, title: string, status: boolean) => {
+const logToDo = (id: number, title: string, status: boolean): void => {
     console.log(`
-    The id is ${id}, 
-    title is "${title}" 
-    and the status of completion is "${status}".
-`);
+        The id is ${id}, 
+        the title is "${title}" 
+        and the status of completion is "${status}".
+    `);
+    console.warn('warning')
 }
 
